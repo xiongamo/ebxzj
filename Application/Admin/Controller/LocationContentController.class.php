780842096758699750faa0b029084bcfbb1ab3d1
xiongamo <1271842745@qq.com>
2016-03-10 23:16:11 +0800

    头部样式修改

---
+++ Application/Admin/Controller/LocationContentController.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace Admin\Controller;
+/**
+ * 主页滚动内容设置
+ * @author mochaokai
+ */
+class LocationController extends BaseController{
+	/**
+	 * 首页
+	 * @author mochaokai
+	 */
+	public function index(){
+		
+	}
+}
