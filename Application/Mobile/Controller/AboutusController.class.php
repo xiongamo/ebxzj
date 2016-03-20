85f337c8b8ba838401533bafc9d60fd7381dac13
xiongamo <1271842745@qq.com>
2016-03-20 12:13:33 +0800

    手机端前端页面基本完成

---
+++ Application/Mobile/Controller/AboutusController.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace Mobile\Controller;
+use Think\Controller;
+/**
+ * 关于我们页
+ * @author mochaokai
+ */
+class AboutusController extends Controller{
+	
+	public function index(){
+		$this->display();
+	}
+}
---
+++ Application/Mobile/Controller/KnowledgeController.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace Mobile\Controller;
+use Think\Controller;
+/**
+ * 保险知识
+ * @author mochaokai
+ */
+class KnowledgeController extends Controller{
+	
+	public function index(){
+		$this->display();
+	}
+}
