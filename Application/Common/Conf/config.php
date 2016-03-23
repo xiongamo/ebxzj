985dfe37235eec18309a583a80dbfd70d2291b8f
xiongamo <1271842745@qq.com>
2016-03-23 23:00:29 +0800

    删除不必要的文件

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
 		'DB_TYPE'   => 'mysql', // 数据库类型
-		'DB_HOST'   => 'qdm129921243.my3w.com' , // 服务器地址
-		'DB_NAME'   => 'qdm129921243_db', // 数据库名
-		'DB_USER'   => 'qdm129921243' , // 用户名
-		'DB_PWD'    => 'ebxzj12345', // 密码
+		'DB_HOST'   => 'rds3n649yws5z6ua455q.mysql.rds.aliyuncs.com' , // 服务器地址
+		'DB_NAME'   => 'rq52i3oi1v', // 数据库名
+		'DB_USER'   => 'rq52i3oi1v' , // 用户名
+		'DB_PWD'    => 'Jinpaibaoxian123', // 密码
 		'DB_PORT'   => 3306, // 端口
 		'DB_PREFIX' => 't_', // 数据库表前缀
 		
